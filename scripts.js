325f05d7aaf752b96d63deda8b9078915f2867bd
Flora Vuong Nu Dong <floravuong@Floras-MacBook-Pro-2.local>
2019-11-08 12:39:40 +0800

    Vinna 'i js

--- scripts.js
+++ scripts.js
@@ -24,28 +24,71 @@ const text = (() => {
   }
 
   // event handler fyrir það að klára færslu
-  function finish(e) {
-  }
+  function finish(e) {}
 
   // event handler fyrir það að breyta færslu
-  function edit(e) {
+  document.getElementsByClassName("item__text").addEventListener("click", myFunction);
+
+  function myFunction() {
+    var x = document.getElementsByClassName("item__text");
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "red";
+    }
   }
+
+  /*
+  document.getElementsByClassName("item__text").addEventListener("click", edit); 
+  function edit() {
+    document.getElementsByClassName("item__text").click = edit () {
+      var span, input, text;
+      // Get the event (handle MS difference)
+      event = event || window.event;
+
+      // Get the root element of the event (handle MS difference)
+      span = event.target || event.srcElement;
+
+      if (span && span.tagName.toUpperCase() === "SPAN") {
+        // Hide it 
+        span.style.display = "none";
+
+        // Get text 
+        text = span.innerHTML;
+
+        // Create an input
+        input = document.createElement("input");
+        input.type = "text";
+        input.value = text;
+        input.size = Math.max(text.lenth / 4 * 3, 4);
+        span.parentNode.insertBefore(input, span);
+
+        // Focus it, hook blur to undo 
+        input.focus();
+        input.onkeypress = function () {
+          // Remove the input
+          span.parentNode.removeChild(input);
+
+          // Update the span
+          span.innerHTML = input.value == "" ? "&nbsp;" : input.value;
+
+          // Show the span again
+          span.style.display = "";
+        };
+      }
+    }
+  }*/
 
   // event handler fyrir það að klára að breyta færslu
-  function commit(e) {
-  }
+  function commit(e) {}
 
   // fall sem sér um að bæta við nýju item
-  function add(value) {
-  }
+  function add(value) {}
 
   // event handler til að eyða færslu
-  function deleteItem(e) {
-  }
+  function deleteItem(e) {}
 
   // hjálparfall til að útbúa element
-  function el(type, className, clickHandler) {
-  }
+  function el(type, className, clickHandler) {}
 
   return {
     init: init
